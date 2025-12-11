b2c959801456e0c24f430956d202cdbb59a36ae3
Connor <connordickie02@gmail.com>
2025-12-11 14:15:42 +0000

    Misc Changes

--- library_project/obj/Debug/net10.0/library_project.AssemblyInfo.cs
+++ library_project/obj/Debug/net10.0/library_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("library_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2811f3944ffab846d355ad4ad864cbe9b45606")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53551a65a3454a3eca3bb4585f00ceb41ce2199d")]
 [assembly: System.Reflection.AssemblyProductAttribute("library_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("library_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
